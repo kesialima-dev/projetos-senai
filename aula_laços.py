a0969966141841cae81281bd8316b9fe706963e7
kesialima-dev <kesialimak0637@gmail.com>
2026-05-04 21:49:14 -0300

    atividade

--- aula_laços.py
+++ aula_laços.py
@@ -1,1 +1,17 @@
+estudante = True
+idoso = int(input("Digite sua idade: "))
+
+if estudante == True or idoso >= 65:
+    print("Ganhou desconto")
+else: 
+    print("Não foi dessa vez!")
+    
+
+
+
+
+
+
+
+
 
